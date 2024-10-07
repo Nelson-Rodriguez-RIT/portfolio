7fc5778e951a87ee784b9714defdcc8b1abf41bc
Nelson-Rodriguez-RIT <njr5833@rit.edu>
2024-10-07 12:22:41 -0400

    Stuffs

---
+++ v2/game-room/ComboWizards/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = 960;
+    canvas.height = 600;
+
+
+    class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        update(){
+
+        }
+
+        draw() {
+            
+        }
+    }
+});
---
+++ v2/game-room/ComboWizards/player.js
@@ -0,0 +1,6 @@
+class Player {
+    constructor(game){
+        this.gane = game;
+        
+    }
+}

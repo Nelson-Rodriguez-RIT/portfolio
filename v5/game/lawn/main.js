3a27487c5cc960c65d6039b97feb5ec3b8164de2
Nelson Rodriguez (RIT Student) <njr5833@rit.edu>
2024-11-06 16:13:31 -0500

    Began l.a.w.n

--- v5/game/lawn/main.js
+++ v5/game/lawn/main.js
@@ -1,5 +1,6 @@
 // Environment setup
-const canvas = document.getElementById('canvas');
+const canvas = document.querySelector('canvas');
+const main = document.querySelector('main');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 1024;
@@ -17,14 +18,43 @@ var Deltatime    = 0;
 let currentTime  = 0;
 let previousTime = 0;
 
-
 const CONFIG = {
-
+    localStorageIdentifier: "njr5833-lawn",
 };
+
+// Variables that need to be identical for each player within a lobby
+const Game = {
+    state: "load",
+    lobbyID: null,
+    players: []
+}
+
+const Cache = {
+    previousState: "load",
+    menu: null,    
+}
+
+// User Setup
+var userID = null;
+
+
 
 
 function onload() {
 
+    if (!(userID = localStorage.getItem(`${CONFIG.localStorageIdentifier}-id`))) {
+        console.log(`No ID token found at "${CONFIG.localStorageIdentifier}-id"! Generating one...`);
+
+        // Generate a random 128 byte token to be used to identify this user amongst other users
+        // within their lobby and for statistics
+        userID = Math.random().toString(32).substring(2) + Math.random().toString(32).substring(2);
+
+        localStorage.setItem(`${CONFIG.localStorageIdentifier}-id`, userID);
+    }
+    console.log(`This session's token is ${userID}`);
+
+
+    Game.state = "menu";
     update();
 }
 
@@ -41,6 +71,76 @@ function update() {
     // Set background
     ctx.fillStyle = 'rgb(36, 36, 36)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Used to deinitialize elements no longer in use at the moment
+    if (Cache.previousState != Game.state && Game.state != "debug") {
+        switch (Cache.previousState) {
+            case "menu": {
+                Cache.menu.title.style.position = "fixed";
+                Cache.menu.title.style.left     = "-9999px";
+
+                Cache.menu.createLobby.style.position = "fixed";
+                Cache.menu.createLobby.style.left     = "-9999px";
+
+                Cache.menu.joinLobby.style.position = "fixed";
+                Cache.menu.joinLobby.style.left     = "-9999px";
+            }
+        }
+    }
+
+    // Updates based on current state (which is controlled by server once the player is in a lobby)
+    switch (Game.state) {
+        case "menu":
+            // Create menu elements if they don't already exist
+            if (Cache.menu == null) {
+                Cache.menu = {};
+
+                // Game title
+                let menuTitle = document.createElement('h1');
+                menuTitle.innerHTML = "Title";
+                menuTitle.className = "js-menu";
+
+                menuTitle.style.position    = "relative";
+                menuTitle.style.textAlign   = "center";
+                menuTitle.style.paddingTop  = "10%"; 
+                menuTitle.style.color       = "rgb(204, 204, 204)";
+
+                Cache.menu.title = menuTitle;
+                main.appendChild(menuTitle);
+
+
+                // TODO: Change state onClick()
+                // Create lobby button
+                let menuCreateLobby = document.createElement('button');
+                menuCreateLobby.innerHTML = "Create Lobby";
+
+                Cache.menu.createLobby = menuCreateLobby;
+                main.appendChild(menuCreateLobby);
+
+
+                // Join lobby button
+                let menuJoinLobby = document.createElement('button');
+                menuJoinLobby.innerHTML = "Join Lobby";
+
+                Cache.menu.joinLobby = menuJoinLobby;
+                main.appendChild(menuJoinLobby);
+            }
+            
+            // Initialize menu options if they haven't been already
+            if (Cache.previousState != "menu") {
+                Cache.menu.title.style.position = "relative";
+                Cache.menu.createLobby.style.position = "relative";
+                Cache.menu.joinLobby.style.position = "relative";
+            }
+
+
+
+
+            Game.state = "debug";
+            break;
+    }
+
+    Cache.previousState = Game.state;
 }
 
 
